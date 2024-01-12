f8ad383a442f9e5e857edbbb6dc2a617ca4f65ec
mi-skam <maksim@miskam.xyz>
2024-01-12 13:48:52 +0100

    adding fullscreen feature

--- src/js/scenes/Game.js
+++ src/js/scenes/Game.js
@@ -6,9 +6,14 @@ export default class Game extends Phaser.Scene {
     super('game');
     // init mediapipe hand tracking
     this.handTracking = new HandTracking({ hands: 2 });
+    this.rightHandUp = false;
   }
 
   preload() {
+    this.load.spritesheet('fullscreen', 'assets/fullscreen.png', {
+      frameWidth: 64,
+      frameHeight: 64,
+    });
     this.load.image('bg', 'assets/Klappbruecke_Test1_BG.jpg');
     this.load.image('bridge', 'assets/Klappbruecke_Test1_Bridge.png');
     this.load.image('level1', 'assets/Klappbruecke_Test1_Ebene1.png');
@@ -17,8 +22,8 @@ export default class Game extends Phaser.Scene {
     this.load.image('displays', 'assets/Klappbruecke_Displays.png');
     this.load.image('arrowUp', 'assets/Klappbruecke_Pfeile_up.png');
     this.load.image('arrowDown', 'assets/Klappbruecke_Pfeile_down.png');
-    this.load.image('handLeft', 'assets/bird.png');
-    this.load.image('handRight', 'assets/bird.png');
+    // this.load.image('handLeft', 'assets/bird.png');
+    // this.load.image('handRight', 'assets/bird.png');
     this.load.image('rope', 'assets/Seil_einzeln2.png');
     this.load.image('car', 'assets/bird.png');
   }
@@ -72,27 +77,21 @@ export default class Game extends Phaser.Scene {
 
     // UI
 
-    this.degrees = this.add
-      .text(width / 2, height / 2, '0', { fontSize: 18 })
-      .setOrigin(0.5);
-
-    this.targetRotation = this.add.text(10, 10, 'Target: 0 (0)', {
-      fontSize: 18,
+    this.targetRotation = this.add.text(10, 10, '0', {
+      fontSize: 24,
     });
 
     // left hand
-    this.leftHandSprite = this.physics.add.sprite(
-      width / 2,
-      height / 2,
-      'handLeft',
-    );
-    this.leftHandSprite.setTint(0xff0000);
+    this.leftHandSprite = this.add.circle(width * 0.5, height * 0.5, 30);
+    this.leftHandSprite.setStrokeStyle(6, 0xe4bfc8);
+
+    this.physics.add.existing(this.leftHandSprite);
+
     // right hand
-    this.rightHandSprite = this.physics.add.sprite(
-      width / 2 + 100,
-      height / 2,
-      'handRight',
-    );
+    this.rightHandSprite = this.add.circle(width * 0.5 + 100, height * 0.5, 30);
+    this.rightHandSprite.setStrokeStyle(6, 0xe4bfc8);
+
+    this.physics.add.existing(this.rightHandSprite);
 
     // add collision between hands and ropes
     this.physics.add.collider(this.leftHandSprite, this.ropes, (_, rope) => {
@@ -102,6 +101,45 @@ export default class Game extends Phaser.Scene {
     this.physics.add.collider(this.rightHandSprite, this.ropes, (_, rope) => {
       this.rightHandCollides = true;
       this.lastRightHandRope = rope; // Store the collided rope
+    });
+
+    const button = this.add
+      .image(800 - 16, 16, 'fullscreen', 0)
+      .setOrigin(1, 0)
+      .setInteractive();
+
+    button.on(
+      'pointerup',
+      function () {
+        if (this.scale.isFullscreen) {
+          button.setFrame(0);
+
+          this.scale.stopFullscreen();
+        } else {
+          button.setFrame(1);
+
+          this.scale.startFullscreen();
+        }
+      },
+      this,
+    );
+
+    // tweens
+
+    this.middleRTweenUp = this.tweens.add({
+      targets: this.middleR,
+      duration: 1000,
+      ease: 'Linear',
+      angle: 90,
+      paused: true,
+    });
+
+    this.middleRTweenDown = this.tweens.add({
+      targets: this.middleR,
+      duration: 1000,
+      ease: 'Linear',
+      angle: 90,
+      paused: true,
     });
   }
 
@@ -145,12 +183,25 @@ export default class Game extends Phaser.Scene {
     ) {
       console.log('right hand gesture: ', rightHandTracked.gesture);
 
-      this.tweens.add({
-        targets: this.middleR,
-        rotation: Math.PI * 0.5,
-        duration: 1000,
-        yoyo: true,
-      });
+      if (!this.rightHandUp) {
+        console.log('bridge is DOWN');
+        if (!this.middleRTweenDown.isPlaying()) {
+          this.middleRTweenDown.reset();
+          this.middleRTweenDown.pause();
+        }
+        this.middleRTweenUp.play();
+        this.rightHandUp = true;
+      }
+
+      if (this.rightHandUp) {
+        console.log('bridge is UP');
+        if (!this.middleRTweenUp.isPlaying()) {
+          this.middleRTweenUp.reset();
+          this.middleRTweenUp.pause();
+        }
+        this.middleRTweenDown.play();
+        this.rightHandUp = false;
+      }
     }
 
     if (
@@ -164,61 +215,9 @@ export default class Game extends Phaser.Scene {
         targets: this.middleL,
         rotation: -Math.PI * 0.5,
         duration: 1000,
-        yoyo: true,
+        yoyo: false,
       });
     }
-  }
-  /**
-   * @param {Phaser.Input.Pointer} pointer
-   */
-
-  handlePointerUp(pointer) {
-    const targetRad = Phaser.Math.Angle.Between(
-      this.middleL.x,
-      this.middleL.y,
-      pointer.x,
-      pointer.y,
-    );
-
-    const currentRad = this.middleL.rotation;
-
-    let diff = targetRad - currentRad;
-
-    if (diff < -Math.PI) {
-      diff += Math.PI * 2;
-    } else if (diff > Math.PI) {
-      diff -= Math.PI * 2;
-    }
-
-    this.tweens.add({
-      targets: this.middleL,
-      rotation: currentRad + diff,
-      duration: 500,
-      onUpdate: tween => {
-        const value = tween.getValue();
-        this.degrees.text = Math.round(Phaser.Math.RadToDeg(value)).toString();
-        this.radians.text = value.toFixed(2);
-      },
-    });
-  }
-  /**
-   *
-   * @param {Phaser.Input.Pointer} pointer
-   */
-
-  handlePointerMove(pointer) {
-    const px = pointer.x;
-    const py = pointer.y;
-
-    const ox = this.middleL.x;
-    const oy = this.middleL.y;
-
-    const targetAngle = Phaser.Math.RadToDeg(
-      Phaser.Math.Angle.Between(ox, oy, px, py),
-    );
-    const target = (targetAngle + 360) % 360;
-
-    this.middleL.angle = target;
   }
 
   updateHandPosition(hand, handData) {
